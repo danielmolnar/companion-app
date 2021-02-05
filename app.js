92f95e862705b82d7c99a7ac890a3e6451f38074
M Schwald <maize@freakmail.de>
2021-02-05 16:48:05 +0100

    unstaged changes

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ for (i = 0; i < wholeTeam.length; i = i + 2) {
   const section = document.createElement("section");
   section.classList.add("team");
   container.appendChild(section);
-  section.innerHTML = `<div class="team__member"> ${wholeTeam[i]} </div>
-                           <img src="/Plus@2x.16876357.png" class='plus'>
-                           <div class="team__member"> ${
-                             wholeTeam[i + 1]
-                           } </div>`;
+  section.innerHTML = `
+  <div class="team__member"> ${wholeTeam[i]} </div>
+  <img src="/Plus@2x.16876357.png" class='plus'>
+  <div class="team__member"> ${wholeTeam[i + 1]} </div>
+  `;
 }
--- teams.js
+++ teams.js
@@ -12,3 +12,20 @@ const wholeTeam = [
 ];
 
 const container = document.querySelector(".main-container");
+
+for (
+  i = 0, a = 0;
+  i < wholeTeam.length, a < wholeTeam.length / 5;
+  i = +5, a++
+) {
+  const section = document.createElement("section");
+  section.classList.add("team");
+  container.appendChild(section);
+  section.innerHTML = `
+    <p class="team__headline">Team + "a" </p>
+    <div class="team__member"> ${wholeTeam[i]} </div>
+    <div class="team__member"> ${wholeTeam[i + 1]} </div>
+    <div class="team__member"> ${wholeTeam[i + 2]} </div>
+    <div class="team__member"> ${wholeTeam[i + 3]} </div>
+    <div class="team__member"> ${wholeTeam[i + 4]} </div>`;
+}
