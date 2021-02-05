da2660888d7d58ca8469cf7ba21f378c4832ba90
M Schwald <maize@freakmail.de>
2021-02-05 12:23:32 +0100

    added JS on buddypage

---
+++ app.js
@@ -0,0 +1,25 @@
+const wholeTeam = [
+  "Andre T.",
+  "Andre W.",
+  "Anita",
+  "Calvin",
+  "Daniel",
+  "Emmanuel",
+  "Karin",
+  "Lea",
+  "Maize",
+  "Marlena",
+];
+
+const container = document.querySelector(".main-container");
+
+for (i = 0; i < wholeTeam.length; i = i + 2) {
+  const section = document.createElement("section");
+  section.classList.add("team");
+  container.appendChild(section);
+  section.innerHTML = `<div class="team__member"> ${wholeTeam[i]} </div>
+                           <img src="/Plus@2x.16876357.png" class='plus'>
+                           <div class="team__member"> ${
+                             wholeTeam[i + 1]
+                           } </div>`;
+}
---
+++ teams.js
@@ -0,0 +1,14 @@
+const wholeTeam = [
+  "Andre T.",
+  "Andre W.",
+  "Anita",
+  "Calvin",
+  "Daniel",
+  "Emmanuel",
+  "Karin",
+  "Lea",
+  "Maize",
+  "Marlena",
+];
+
+const container = document.querySelector(".main-container");
