ad437171e1bfe0375186d19738ff2b53eda0b027
Anita Schiller <anita.s1@gmx.de>
2021-02-07 16:32:15 +0100

    Created general solution for journal-new stars and comprehension

--- journal.js
+++ journal.js
@@ -1,135 +1,53 @@
-// const imgURL1 = 'assets/Star.svg'
-// const imgURL2 = "assets/Star Copy 8@2x.png"
-// //const img = document.querySelector('img');
-//const star = document.getElementById('Stern1');
+//STARS
 
-// img.src = imgURL1;
+const stars = document.querySelectorAll('.star')
+const starsArr = Array.from(stars)
+let untilStar
 
-//star.addEventListener('click', () => {
-//    console.log('test');
-//)}
- 
+starsArr.forEach(fillStars)
 
+function fillStars(star, i) {
+  star.addEventListener("click", () => {
 
-const star1 = document.getElementById("Stern1");
-const star2 = document.getElementById("Stern2");
-const star3 = document.getElementById("Stern3");
-const star4 = document.getElementById("Stern4");
-const star5 = document.getElementById("Stern5");
+    starsArr.forEach((starArrowItem) => {
+      starArrowItem.classList.remove('star__full')
+    })
 
+    star.classList.add('star__full')
 
+    untilStar = starsArr.slice(0, i)
 
-
-var state = 0;
-
-star1.addEventListener("click", () => {
-
-
-
-if (state == 0 ) {
-star1.classList.add("star__full");
-state = 1;
-} else {
-    star1.classList.remove("star__full");
-    star2.classList.remove("star__full");
-    star3.classList.remove("star__full");
-    star4.classList.remove("star__full");
-    star5.classList.remove("star__full");
-   
-    state = 0;
+    untilStar.forEach((untilStarItem) => {
+      untilStarItem.classList.add('star__full')
+    })
+  })
 }
-})
 
+//COMPREHENSION
 
-star2.addEventListener("click", () => {
+const squares = document.querySelectorAll('.square')
+const squaresArr = Array.from(squares)
+let untilSquare
 
-if (state == 0) {
-    star1.classList.add("star__full");
-    star2.classList.add("star__full");
-   
-  state = 1;
-} else {
-    star1.classList.remove("star__full");
-    star2.classList.remove("star__full");
-    star3.classList.remove("star__full");
-    star4.classList.remove("star__full");
-    star5.classList.remove("star__full");
-  state = 0;
+squaresArr.forEach(fillSquares)
+
+function fillSquares(square, i) {
+  square.addEventListener('click', () => {
+
+    squaresArr.forEach((squaresArrowItem) => {
+      squaresArrowItem.classList.remove('comprehension__full')
+    })
+
+    square.classList.add('comprehension__full')
+
+    untilSquare = squaresArr.slice(0, i)
+
+    untilSquare.forEach((untilSquareItem) => {
+      untilSquareItem.classList.add('comprehension__full')
+    })
+  })
 }
-});
 
-for (i = 0; i < 6; i++)
-
-
-
-star3.addEventListener("click", () => {
-  if (state == 0) {
-    star1.classList.add("star__full");
-    star2.classList.add("star__full");
-    star3.classList.add("star__full");
-
-    state = 1;
-  } else {
-    star1.classList.remove("star__full");
-    star2.classList.remove("star__full");
-    star3.classList.remove("star__full");
-    star4.classList.remove("star__full");
-    star5.classList.remove("star__full");
-    state = 0;
-  }
-});
-
-star4.addEventListener("click", () => {
-  if (state == 0) {
-    star1.classList.add("star__full");
-    star2.classList.add("star__full");
-    star3.classList.add("star__full");
-    star4.classList.add("star__full");
-
-    state = 1;
-  } else {
-    star1.classList.remove("star__full");
-    star2.classList.remove("star__full");
-    star3.classList.remove("star__full");
-    star4.classList.remove("star__full");
-    star5.classList.remove("star__full");
-    state = 0;
-  }
-});
-
-star5.addEventListener("click", () => {
-  if (state == 0) {
-    star1.classList.add("star__full");
-    star2.classList.add("star__full");
-    star3.classList.add("star__full");
-    star4.classList.add("star__full");
-    star5.classList.add("star__full");
-
-    state = 1;
-  } else {
-    star1.classList.remove("star__full");
-    star2.classList.remove("star__full");
-    star3.classList.remove("star__full");
-    star4.classList.remove("star__full");
-    star5.classList.remove("star__full");
-    state = 0;
-  }
-});
-
-// star4.addEventListener("click", () => {
-//   star1.classList.toggle("star__full");
-//   star2.classList.toggle("star__full");
-//     star3.classList.toggle("star__full");
-//     star4.classList.toggle("star__full");
-// });
-
-// star5.addEventListener("click", () => {
-// star1.classList.toggle("star__full");
-// star2.classList.toggle("star__full");
-// star3.classList.toggle("star__full");
-// star4.classList.toggle("star__full");
-// star5.classList.toggle("star__full");
-// });
 
 
 
