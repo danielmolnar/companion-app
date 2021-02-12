7461a93c203e55c9292753e84b884026f6d7b7e8
Anita Schiller <anita.s1@gmx.de>
2021-02-12 18:00:40 +0100

    Friday exercise

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-const wholeTeam = [
+
+fetch('https://muc-2020-w1-student-api.vercel.app/api/buddies')
+  .then((res) => res.json())
+  .then((buddies) => buddies.forEach(renderTeam))
+
+
+const container = document.querySelector(".main-container");
+
+function renderTeam(buddy) {
+  const team = document.createElement('section')
+  team.classList.add('team')
+  container.appendChild(team)
+  team.innerHTML = `<div class="team__member"> ${buddy[0]} </div>
+                           <img src="/Plus@2x.16876357.png" class='plus'>
+                           <div class="team__member"> ${buddy[1]} </div>`
+}
+
+
+/* const wholeTeam = [
   "Andre T.",
   "Andre W.",
   "Anita",
@@ -22,3 +40,4 @@ for (i = 0; i < wholeTeam.length; i = i + 2) {
                            <div class="team__member"> ${wholeTeam[i + 1]
     } </div>`;
 }
+ */
